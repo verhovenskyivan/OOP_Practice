e781345123a927b61c44ec5497190b514778c6c2
verhovensky <verhovenskyivan@gmail.com>
2022-12-03 20:50:07 +0300

    practiced oop

---
+++ OOP/Character.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+     class Character
+    {
+      public int Health = 100;
+
+      public void Hit(int damage)
+        {
+            Health -= damage;
+        }
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args )
+        {
+            Character c = new Character();
+            c.Hit(10);
+
+            Console.WriteLine(c.Health);
+        }
+    }
+}
