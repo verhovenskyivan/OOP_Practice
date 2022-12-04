afe46947a46748ba1efaaf84692e6d5f77d148ca
verhovensky <verhovenskyivan@gmail.com>
2022-12-04 10:06:04 +0300

    practiced exceptions in oop with triangle parameters

--- OOP/Calculator.cs
+++ OOP/Calculator.cs
@@ -20,5 +20,10 @@ namespace OOP
         {
             return 0.5 * b * h;
         }
+        public double CalcTriangleSquare(double ab, double ac, int alpha)
+        {
+            double rads = alpha * Math.PI / 180;
+            return 0.5 * ab * ac * Math.Sin(alpha);
+        }
     }
 }
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -9,9 +9,10 @@ namespace OOP
         {
             Calculator calc = new Calculator();
             double square1 = calc.CalcTriangleSquare(10, 20);
-            double square2 =calc.CalcTriangleSquare(10, 20, 30);
+            double square2 = calc.CalcTriangleSquare(10, 20, 30);
+            double square3 = calc.CalcTriangleSquare(10, 20, 50);
 
-            Console.WriteLine($"Square1={square1}. Square2={square2}");
+            Console.WriteLine($"Square1={square1}. Square2={square2}. Square3={square3}");
         }
     }
 }
